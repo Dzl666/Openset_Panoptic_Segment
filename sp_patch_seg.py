fce6e627340d6d8fdb0293519a84ee49532a5b10
Dzl666 <deng_zilong@outlook.com>
2025-04-10 11:55:30 +0200

    update super pixel segment

--- sp_patch_seg.py
+++ sp_patch_seg.py
@@ -81,9 +81,9 @@ def main():
 
     num_frame = len(file_names)
     step = 5
-    num_frame = 40 * step
+    num_frame = 2 * step
 
-    encoder = 'siglip' # 'clip' or 'siglip'
+    encoder = 'clip' # 'clip' or 'siglip'
 
     exp_name = f'sp_{encoder}'
     result_dir = pjoin('results', seq_name)
@@ -94,7 +94,7 @@ def main():
 
     if encoder == 'clip':
         # feat_dim = 512 # for ViT-B/32
-        # feat_dim = 768 # for ViT-L/14
+        feat_dim = 768 # for ViT-L/14
         clip_model, prep_clip = create_clip_extractor('ViT-L/14', device=DEVICE)
         text_inputs = clip.tokenize(text_cands, context_length=77).to(DEVICE)
         with torch.no_grad():
@@ -112,19 +112,108 @@ def main():
         with torch.no_grad():
             text_feats = siglip_model.get_text_features(**text_inputs)
         
-    # text_feats = text_feats.detach().cpu().to(torch.float32).numpy()
-
+    text_feats = text_feats.detach().cpu() #.to(torch.float32).numpy()
 
     cos_sim_func = torch.nn.CosineSimilarity(dim=-1)
+
+    # crop the whole image into multiple scale of patches
+    crop_size = [(H, W), (H//2, W//2), (H//4, W//4)]
+    clip_p_size = 14
+    clip_ptk_num = 16
+    # each patch is 14 x 14 cropped from the 224 x 224 image after downsampling
+    patch_map = np.zeros((clip_p_size*clip_ptk_num, clip_p_size*clip_ptk_num), dtype=np.uint8)
+    for patch_idx in range(clip_ptk_num * clip_ptk_num):
+        _row = (patch_idx // clip_ptk_num)
+        _col = patch_idx - _row * clip_ptk_num
+        _x = _col * clip_p_size
+        _y = _row * clip_p_size
+        # set the patch map to 1
+        patch_map[_y:_y+clip_p_size, _x:_x+clip_p_size] = patch_idx
     
+
     for f_idx in tqdm(range(0, num_frame, step)):
         rgb_file = pjoin(rgb_path, file_names[f_idx])
         rgb = cv2.imread(rgb_file)
         rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
 
-        lab = cv2.cvtColor(rgb, cv2.COLOR_RGB2LAB)
+        feat_map = np.zeros((H, W, feat_dim), dtype=np.float32)
 
-        slic_segs = segmentation.slic(lab, n_segments=150, compactness=10, sigma=1)
+        for scale_layer in range(1):
+            crop_h, crop_w = crop_size[scale_layer]
+
+            layer_feat_map = np.zeros_like(feat_map, dtype=np.float32)
+            map_cnt = np.zeros((H, W), dtype=np.int16)
+
+            # use sliding window to crop the image
+            stride_h = int(crop_h // 2)
+            stride_w = int(crop_w // 2)
+            for i in range(0, H - crop_h + 1, stride_h):
+                for j in range(0, W - crop_w + 1, stride_w):
+                    # crop the image
+                    rgb_patch = rgb[i:i + crop_h, j:j + crop_w, :]
+                    cls_tk, ptks = clip_model.encode_image(prep_clip(Image.fromarray(rgb_patch)).unsqueeze(0).to(DEVICE))
+                    ptks = ptks.squeeze().detach().cpu().numpy()
+                    
+
+                    lab_patch = cv2.cvtColor(rgb_patch, cv2.COLOR_RGB2LAB)
+                    slic_segs = segmentation.slic(lab_patch, n_segments=50, compactness=10, sigma=2)
+                    seg_idx, seg_area = np.unique(slic_segs, return_counts=True)
+
+                    resize_patch_map = cv2.resize(patch_map, (crop_w, crop_h), interpolation=cv2.INTER_NEAREST)
+                    patch_size = crop_w* crop_h / clip_ptk_num / clip_ptk_num
+
+                    feat_crop = np.zeros((crop_h, crop_w, feat_dim), dtype=np.float32)
+                    # find the idx of patches covered by each slis seg
+                    for slic_id in seg_idx:
+                        patch_cover = resize_patch_map[slic_segs == slic_id]
+                        patch_idx, patch_area = np.unique(patch_cover, return_counts=True)
+                        # remove the id with area < 10% of each patch
+                        patch_idx = patch_idx[patch_area > 0.1 * patch_size]
+                        patch_feat = np.mean(ptks[patch_idx], axis=0)
+                        feat_crop[slic_segs == slic_id] = patch_feat
+
+                    layer_feat_map[i:i + crop_h, j:j + crop_w, :] += feat_crop
+                    map_cnt[i:i + crop_h, j:j + crop_w] += 1
+
+            map_cnt[map_cnt == 0] = 1
+            layer_feat_map /= map_cnt[:, :, np.newaxis].astype(np.float32)
+            feat_map += layer_feat_map
+        
+        # feat_map /= len(crop_size)
+        feat_map = torch.from_numpy(feat_map)
+
+        print("Visualizing the segmentation results")
+        per_pixel_sim = torch.zeros((H, W, len(text_cands)), dtype=torch.float32)
+        for i in range(len(text_cands)):
+            text_feat = text_feats[i]
+            _sim = cos_sim_func(feat_map, text_feat.unsqueeze(0).unsqueeze(0))
+            per_pixel_sim[:, :, i] = _sim.squeeze(-1)
+
+        # get the best match id for each pixel
+        best_match = torch.argmax(per_pixel_sim, dim=-1)  # (H, W)
+
+        disp_img = torch.zeros((H, W, 3)).float()
+        pallete = get_new_pallete(len(text_cands))
+        for _i in range(len(text_cands)):
+            disp_img[best_match == _i] = pallete[_i]
+    
+        disp_img = 0.5 * disp_img.detach().cpu().numpy() + 0.5 * rgb.astype('float32') / 255.0
+    
+        fig = plt.figure(figsize=(15, 15))
+        plt.imshow(disp_img)
+        plt.axis('off')
+        plt.legend(handles=[
+            mpatches.Patch(
+                color=(pallete[i][0].item(),
+                    pallete[i][1].item(), pallete[i][2].item(),),
+                label=text_cands[i],
+            )
+            for i in range(len(text_cands))], 
+            loc='upper left', 
+            bbox_to_anchor=(1.05, 1), 
+            borderaxespad=0.0,
+        )
+        plt.savefig(f"results/sp_seg_{f_idx}.png")
 
         # ### save the segmentation results
         # save_pkl_path = pjoin(result_dir, 'openseg_sp', str(f_idx).zfill(5)+'.pkl')
